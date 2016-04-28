9706600168f50f577a8dc073e38e086e3c61b8fa
Demitrious Kelly <demitrious@automattic.com>
2016-04-28 10:46:42 -0700

    add -prefix={text} where {text} will be inserted between the timestamp and the line

--- main.go
+++ main.go
@@ -2,21 +2,30 @@ package main
 
 import (
 	"bufio"
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
 )
 
+var prefix = ""
+
 func main() {
+	flag.StringVar(&prefix, "prefix", prefix, "insert this text after the timestamp but before the input line")
+	flag.Parse()
+	if prefix != "" {
+		prefix = prefix + " "
+	}
 	log.SetOutput(os.Stdout)
 	log.SetFlags(log.Ldate | log.Lmicroseconds)
 	stdin := bufio.NewReader(os.Stdin)
 	for {
 		if line, err := stdin.ReadString('\n'); err == nil {
-			log.Print(line)
+			log.Print(fmt.Sprintf("%s%s", prefix, line))
 		} else {
 			if line != "" {
-				log.Print(line)
+				log.Print(fmt.Sprintf("%s%s", prefix, line))
 			}
 			if err != io.EOF {
 				log.Fatal(err)
