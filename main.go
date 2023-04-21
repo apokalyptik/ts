fafb5735159c8679f623cf27531935d899bc0526
Demitrious Kelly <apokalyptik@apokalyptik.com>
2023-04-21 10:47:36 -0700

    allow deltas. remove prefix (not very useful)

--- main.go
+++ main.go
@@ -7,25 +7,41 @@ import (
 	"io"
 	"log"
 	"os"
+	"time"
 )
 
-var prefix = ""
+var (
+	deltas = false
+)
+
+func init() {
+	flag.BoolVar(&deltas, "deltas", deltas, "print deltas")
+}
 
 func main() {
-	flag.StringVar(&prefix, "prefix", prefix, "insert this text after the timestamp but before the input line")
 	flag.Parse()
-	if prefix != "" {
-		prefix = prefix + " "
-	}
 	log.SetOutput(os.Stdout)
 	log.SetFlags(log.Ldate | log.Lmicroseconds)
 	stdin := bufio.NewReader(os.Stdin)
+	now := time.Now()
+	start := now
 	for {
+		delta := ""
 		if line, err := stdin.ReadString('\n'); err == nil {
-			log.Print(fmt.Sprintf("%s%s", prefix, line))
+			if deltas {
+				newNow := time.Now()
+				delta = fmt.Sprintf("d:%f D:%f ", newNow.Sub(now).Seconds(), newNow.Sub(start).Seconds())
+				now = newNow
+			}
+			log.Print(fmt.Sprintf("%s%s", delta, line))
 		} else {
 			if line != "" {
-				log.Print(fmt.Sprintf("%s%s", prefix, line))
+				if deltas {
+					newNow := time.Now()
+					delta = fmt.Sprintf("d:%f D:%f ", newNow.Sub(now).Seconds(), newNow.Sub(start).Seconds())
+					now = newNow
+				}
+				log.Print(fmt.Sprintf("%s%s", delta, line))
 			}
 			if err != io.EOF {
 				log.Fatal(err)
